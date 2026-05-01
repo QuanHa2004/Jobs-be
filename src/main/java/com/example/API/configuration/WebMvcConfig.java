5b417f89debba5f101e100fa8531aa2f304e3aa8
QuanHa2004 <hahongquan2004@gmail.com>
2026-05-01 18:36:23 +0700

    tao bien moi truong

--- src/main/java/com/example/API/configuration/WebMvcConfig.java
+++ src/main/java/com/example/API/configuration/WebMvcConfig.java
@@ -1,16 +1,19 @@
 package com.example.API.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
